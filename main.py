01dd0951f2910107ebb3524f5fe2dfd5bb0639c8
Alexey Popkov <AlexeyPopkov@pm.me>
2024-05-29 16:11:00 +0300

    try get client ip

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     asyncio.run(on_startup())
-    uvicorn.run(app=app, host="0.0.0.0", port=8000, proxy_headers=True)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000, proxy_headers=True, forwarded_allow_ips="*")
